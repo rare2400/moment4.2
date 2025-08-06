81ea376fee1c7b0e2f345f5927c28e76ec590b10
Ramona <rare2400@student.miun.com>
2025-08-06 10:39:21 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Frontend-applikation med användarhantering
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+/** Moment 4 DT207G
+ * Frontend-applikation med användarhantering
+ * Av Ramona Reinholdz
+ * rare2400
+ */
+
+"use strict";
